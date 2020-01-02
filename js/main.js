340cd4425a7bce8a3e3efd7d9eba0874087ebf5b
quinsberry <fatbobisreal@gmail.com>
2020-01-02 11:22:58 +0100

    Added prj in new repository

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+const responsive = {
+  0: {
+    items: 1
+  },
+  320: {
+    items: 1
+  },
+  560: {
+    items: 2
+  },
+  960: {
+    items: 3,
+    center: true
+  },
+  1600: {
+    items: 4
+  }
+}
+
+$(document).ready(function () {
+  $nav = $('.nav');
+  $toggleCollapse = $('.toggle-collapse');
+
+  /* click event on toggle menu */
+  $toggleCollapse.click(function () {
+    $nav.toggleClass('collapse');
+  });
+
+  // owl-carousel for blog 
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+  });
+
+  // click to scroll top
+  $('.move-up span').click(() => {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+
+  // AOS Instance
+  AOS.init();
+
+
+});
